106bf8c7d6c526cb2a6c256b68ba2face3aef918
Ksenia Mironova <kseniamironova@MacBook-Pro-de-Ksenia.local>
2018-10-24 11:22:29 +0200

    first commit test

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ switch($_POST['action']) {
 
     $id = $_POST['id'];
     if(is_numeric($id)) {
-      $updateQuery = ''; // IMPLEMENT ME
+      $updateQuery = 'test'; // IMPLEMENT ME
       if(!$db->query($updateQuery)) {
         die(print_r($db->errorInfo(), true));
       }
